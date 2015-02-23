5d3f0325805707c7066fbd04ac87067278612257
pauloecmarcal <pauloecmarcal@gmail.com>
2015-02-23 12:00:32 -0300

    mensagem de teste 1 e 2 no main activity REMOVIDAS

--- app/src/main/java/app/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/app/com/example/android/sunshine/MainActivity.java
@@ -61,8 +61,6 @@ public class MainActivity extends ActionBarActivity {
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
-            //Teste Git
-            // Teste Git 2
         }
     }
 }
